9d6c4564223c32f30858045990fe14a8b110d684
fntp <46984923+fntp@users.noreply.github.com>
2026-02-24 02:30:27 +0000

    1

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '多一点兴趣，少一点功利', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://polofox.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.polofox.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '码农修炼笔记,码农,修炼笔记,fntp,码农博客,码农的笔记,码农的博客,码农的修炼笔记,码农的修炼,polofox,polo,javaFX,Fntp教程,Fntp笔记', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
